6c114138f8981d742efb4d701934ea65b2587c95
Tushar Bhavsar <148482277+TusharBhavsar08@users.noreply.github.com>
2025-04-01 13:07:19 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+//alert(3+3) //we are using node js, not browser
+console.log (3+3) // code readability 
+console.log ("Tushar")
+
+let name = "tushar"
+let age = 23
+let isloggedIn = false
+
+/* 
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+object IMPORTANT
+
+*/
+
+console.log(typeof age);
+jj
+
